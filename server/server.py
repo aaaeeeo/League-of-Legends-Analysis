651134bbda632f5e19ea2040ab31e74683aade85
aaaeeeo <lee.zuoming@gmail.com>
2016-11-26 12:18:24 -0600

    fix

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
-from bottle import get, run, post, request, static_file
+from bottle import get, run, post, request, static_file, response
+from gevent import monkey
 import happybase
-import collections
 import json
+import StringIO
+import gzip
+monkey.patch_all()
 
-connection = happybase.Connection('hdp-m.c.mpcs53013-2016.internal', port=9090)
 
 @get('/')
 def index():
@@ -15,6 +17,7 @@ def send_image(filename):
 
 @post('/pos')
 def pos():
+    connection = happybase.Connection('hdp-m.c.mpcs53013-2016.internal', port=9090)
     connection.open()
     table = connection.table('match_pos')
     tablesp = connection.table('match_pos_speed')
@@ -36,8 +39,10 @@ def pos():
     combs = recur([""], para_list)
 
     json_str = ""
+    json_str_sp = ""
     count = 0
-    max_val = 0
+    count_sp = 0
+    max_val = 1
     for comb in combs:
         row = table.row(comb)
         rowsp = tablesp.row(comb)
@@ -46,14 +51,21 @@ def pos():
             json_str += (',' if len(json_str)>0 else '') + row['pos:str']
             max_val += int(row['pos:max'])
         if len(rowsp)>0:
-            count += 1
-            json_str += (',' if len(json_str)>0 else '') + rowsp['pos:str']
-    print count
+            count_sp += 1
+            json_str_sp += (',' if len(json_str_sp)>0 else '') + rowsp['pos:str']
+    print count, count_sp
+    res = '{"max":'+str(max_val*0.05) + ',"data":['+ (json_str if count>0 else json_str_sp) +']}'
     connection.close()
-    return '{"max":'+str(max_val*0.05)+',"data":['+json_str+']}'
+    out = StringIO.StringIO()
+    with gzip.GzipFile(fileobj=out, mode="w") as f:
+      f.write(res)
+    response.content_type = 'application/json'
+    response.set_header('Content-Encoding', 'gzip')
+    return out.getvalue()
 
 @post('/pos_all')
 def pos_all():
+    connection = happybase.Connection('hdp-m.c.mpcs53013-2016.internal', port=9090)
     connection.open()
     table = connection.table('match_pos')
     tablesp = connection.table('match_pos_speed')
@@ -63,7 +75,13 @@ def pos_all():
     val = row['pos:str']
     if len(rowsp)>0:
         val += ','+rowsp['pos:str']
+    res = '{"max":'+str(int(row['pos:max'])*0.8)+',"data":['+val+']}'
     connection.close()
-    return '{"max":'+str(int(row['pos:max'])*0.8)+',"data":['+val+']}'
+    out = StringIO.StringIO()
+    with gzip.GzipFile(fileobj=out, mode="w") as f:
+      f.write(res)
+    response.content_type = 'application/json'
+    response.set_header('Content-Encoding', 'gzip')
+    return out.getvalue()
 
-run(host='0.0.0.0', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, debug=True, server='gevent')
